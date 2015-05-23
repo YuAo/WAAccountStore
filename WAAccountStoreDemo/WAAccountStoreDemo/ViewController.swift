2b181740f54f9736d7b9da1989480501b7eb350e
YuAo <me@imyuao.com>
2015-05-23 15:22:07 +0800

    add demo project

---
+++ WAAccountStoreDemo/WAAccountStoreDemo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  WAAccountStoreDemo
+//
+//  Created by YuAo on 5/23/15.
+//  Copyright (c) 2015 YuAo. All rights reserved.
+//
+
+import UIKit
+import WAAccountStore
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
